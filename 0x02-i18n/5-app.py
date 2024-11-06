c458e8507839a9ef1d6c91bce1c44eb076174d4b
DJAMAA Ryma <88832599+rimadjamaa@users.noreply.github.com>
2024-11-06 18:09:56 +0000

    task 5✅

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""A Basic Flask app with internationalization support.
+"""
+from flask_babel import Babel
+from typing import Union, Dict
+from flask import Flask, render_template, request, g, jsonify
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+@app.route('/check-params')
+def check_params() -> str:
+    """Checks and returns all query parameters as JSON."""
+    # Convert query parameters to a dictionary
+    params = request.args.to_dict()
+    # Return all parameters as JSON
+    return jsonify(params=params)
+
+def get_user() -> Union[Dict, None]:
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.before_request
+def before_request() -> None:
+    """Performs some routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
+
+
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+# Manually set the locale selector function for Flask-Babel
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
